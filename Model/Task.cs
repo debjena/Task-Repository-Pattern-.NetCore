c91f0febd028a3658b283314e782e4587db91d10
Debabrata Jena <djena3@dxc.com>
2019-07-27 22:39:49 +0530

    data changes

--- Model/Task.cs
+++ Model/Task.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Task_Repo_Pattern.Model
 {
     public class Task
     {
         public long Id { get; set; }
+        [Required(ErrorMessage="Please enter task name.")]
         public string TaskName { get; set; }
-        public bool IsDone { get; set; }
+        [DataType(DataType.Date)]
+        [JsonConverter(typeof(IsoDateConverter))]
+        public DateTime TaskDate { get; set; }
+        public bool IsTaskDone { get; set; }
+    }
+    public class IsoDateConverter : IsoDateTimeConverter
+    {
+    public IsoDateConverter() => 
+        this.DateTimeFormat = "MM/dd/yyyy";
     }
 }
